1511ae79f3368e145fb836f46614fae0198a48e3
2010jmatpon <jmatpon2010@g.educaand.es>
2023-10-11 10:42:31 +0200

    Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  num1: number = 0;
+  num2: number = 0;
+  suma: number = 0;
+  resta: number = 0;
+  producto: number = 0;
+  division: number = 0;
+  txtMensaje: string = '';
+  esCorrecto: boolean = false;
 
-  constructor() {}
+  constructor() {
+    this.sumar();
+    this.restar();
+    this.multiplicar();
+    this.dividir();
+  }
 
+  sumar() {
+    this.suma = this.num1 + this.num2;
+    this.txtMensaje = 'El resultado es: ' + this.suma;
+  }
+
+  restar() {
+    this.resta = this.num1 - this.num2;
+    this.txtMensaje = 'El resultado es: ' + this.resta;
+  }
+
+  multiplicar() {
+    this.producto = this.num1 * this.num2;
+    this.txtMensaje = 'El resultado es: ' + this.producto;
+  }
+
+  dividir() {
+    this.division = this.num1 / this.num2;
+    this.txtMensaje = 'El resultado es: ' + this.division;
+  }
 }
